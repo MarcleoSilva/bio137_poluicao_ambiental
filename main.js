b939dcfe325e7b78ca5c2cca5da42e55fd6675c8
Marcelo Araújo Silva <marceloaraujosilva739@gmail.com>
2025-11-15 11:19:56 -0300

    calculation prototype: made the event listener and separated the space to create the calculation area

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-// Calculator script
+// Slide bars
+const valueMeat = document.querySelector("#carne_value"); //output
+const inputMeat = document.querySelector("#carne"); //input
+
+valueMeat.textContent = inputMeat.value;
+
+inputMeat.addEventListener("inputMeat", (event) => {
+    valueMeat.textContent = event.target.value;
+});
+
+
+
+// Result calculation
+const outputCalculation = document.querySelector("#results")
+const calculationOfConsumption = ((inputMeat.value + 10) * 100) / 2
+
+outputCalculation.textContent = calculationOfConsumption;
+calculationOfConsumption.addEventListener("calculationOfConsumption", (event) => {
+    outputCalculation.textContent = event.target.value;
+});
